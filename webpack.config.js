551ad5bb08c4b5f944aad5d4a3a5ab3312cbc962
jefferson.souza <jefferson.souza@synchro.com.br>
2019-01-30 14:27:16 -0200

    setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const path = require("path");
+
+module.exports = {
+  entry: "./src/NumericInput.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+    libraryTarget: "commonjs2",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};
